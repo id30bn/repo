7c6d3ee6ce293d9f1c4d22b1dfd005dbb92f2844
Ihar <ihar_dubina@epam.com>
2024-01-12 13:57:13 +0300

    add cartId to ItemMessage, add bi-directional streaming flow

--- Carting/Carting/API/gRPC/CartItemsGrpcService.cs
+++ Carting/Carting/API/gRPC/CartItemsGrpcService.cs
@@ -18,9 +18,13 @@ namespace Carting.API.gRPC
 
 		public override Task<CartItemsReplyMessage> GetCartItemsUnary(CartItemsRequestMessage request, ServerCallContext context)
 		{
-			return Task.FromResult(
-					FormCartReplyMessage(request.CartId, _cartingService.GetCartItems(request.CartId))
+			var reply = new CartItemsReplyMessage() { CartId = request.CartId };
+			var mappedItems = _mapper.Map<IEnumerable<Item>, IEnumerable<ItemMessage>>(
+				_cartingService.GetCartItems(request.CartId)
 				);
+			reply.Items.AddRange(mappedItems);
+
+			return Task.FromResult(reply);
 		}
 
 		public override async Task GetCartItemsServerStreaming(CartItemsRequestMessage request, IServerStreamWriter<ItemMessage> responseStream, ServerCallContext context)
@@ -31,41 +35,54 @@ namespace Carting.API.gRPC
 			foreach ( var item in mappedItems ) {
 				await responseStream.WriteAsync( item );
 
-				await Task.Delay(TimeSpan.FromSeconds(2));
+				await Task.Delay(TimeSpan.FromSeconds(1));
 			}
 		}
 
-		public override async Task<CartItemsReplyMessage> AddItemToCartClientStreaming(
-			IAsyncStreamReader<AddItemToCartRequestMessage> requestStream,
+		public override async Task<AddItemsReplyMessage> AddItemToCartClientStreaming(
+			IAsyncStreamReader<ItemMessage> requestStream,
 			ServerCallContext context)
 		{
-			int cartId = -1;
-			await foreach (AddItemToCartRequestMessage request in requestStream.ReadAllAsync()) {
-				if (cartId == -1) {
-					cartId = request.CartId;
-				}
-
-				if (cartId == request.CartId) {
-					AddItemToCart(request.CartId, _mapper.Map<Item>(request.Item));
-				}
+			int count = 0;
+			await foreach (var itemMessage in requestStream.ReadAllAsync()) {
+				AddItemToCart(itemMessage.CartId, _mapper.Map<Item>(itemMessage));
+				count++;
 			}
 
-			return await Task.FromResult(
-					FormCartReplyMessage(cartId, _cartingService.GetCartItems(cartId))
-				);
+			return new AddItemsReplyMessage { AddedItemsCount = count };
 		}
+
+		public override async Task AddItemToCartBiDirectionalStreaming(IAsyncStreamReader<ItemMessage> requestStream, IServerStreamWriter<ItemMessage> responseStream, ServerCallContext context)
+		{
+			await foreach (var itemMessage in requestStream.ReadAllAsync()) {
+				AddItemToCart(itemMessage.CartId, _mapper.Map<Item>(itemMessage));
+				await responseStream.WriteAsync(itemMessage);
+			}
+		}
+
+		//public override async Task<CartItemsReplyMessage> AddItemToCartClientStreaming(
+		//	IAsyncStreamReader<AddItemToCartRequestMessage> requestStream,
+		//	ServerCallContext context)
+		//{
+		//	int cartId = -1;
+		//	await foreach (AddItemToCartRequestMessage request in requestStream.ReadAllAsync()) {
+		//		if (cartId == -1) {
+		//			cartId = request.CartId;
+		//		}
+
+		//		if (cartId == request.CartId) {
+		//			AddItemToCart(request.CartId, _mapper.Map<Item>(request.Item));
+		//		}
+		//	}
+
+		//	return await Task.FromResult(
+		//			FormCartReplyMessage(cartId, _cartingService.GetCartItems(cartId))
+		//		);
+		//}
 
 		private void AddItemToCart(int cartId, Item item)
 		{
 			_cartingService.AddItemToCart(cartId, item);
-		}
-
-		private CartItemsReplyMessage FormCartReplyMessage(int cartId, IEnumerable<Item> items)
-		{
-			var reply = new CartItemsReplyMessage() { CartId = cartId };
-			var mappedItems = _mapper.Map<IEnumerable<Item>, IEnumerable<ItemMessage>>(items);
-			reply.Items.AddRange(mappedItems);
-			return reply;
 		}
 	}
 }
--- GrpcClient/GrpcClient/Program.cs
+++ GrpcClient/GrpcClient/Program.cs
@@ -1,33 +1,49 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using GrpcClient;
 
 using var channel = GrpcChannel.ForAddress("https://localhost:7284");
 var client = new CartItemsService.CartItemsServiceClient(channel);
 
-// client streaming
-Console.WriteLine("Client streaming flow:");
-var itemsToAdd = Enumerable.Range(51, 4).Select(x => new ItemMessage { Id = x, Name = $"custom name {x}" });
-var requestCall = client.AddItemToCartClientStreaming();
-foreach(var item in itemsToAdd) {
-	await requestCall.RequestStream.WriteAsync(new AddItemToCartRequestMessage { 
-		CartId = 10,
-		Item = item
-	});
-	// to test that it will be skipped
-	await requestCall.RequestStream.WriteAsync(new AddItemToCartRequestMessage {
-		CartId = 4,
-		Item = itemsToAdd.First()
-	});
+#region bi-directional streaming
+Console.WriteLine("Bi-directional streaming flow:");
+var itemsToAdd = Enumerable.Range(20, 4).Select(x => new ItemMessage { Id = x, Name = $"custom name {x}", CartId = 5 });
+var call = client.AddItemToCartBiDirectionalStreaming();
+
+var readTask = Task.Run(async () => {
+	await foreach (var item in call.ResponseStream.ReadAllAsync()) {
+		Console.WriteLine($"Item {item.Id} was added to cart {item.CartId}");
+	}
+});
+
+foreach (var item in itemsToAdd) {
+	await call.RequestStream.WriteAsync(item);
+	await Task.Delay(2000);
 }
 
-await requestCall.RequestStream.CompleteAsync();
+await call.RequestStream.CompleteAsync(); // important
+await readTask; // await after all messages were sent
+#endregion bi-directional streaming
 
-CartItemsReplyMessage clientStreamingResponse = await requestCall.ResponseAsync;
+#region client streaming
+//Console.WriteLine("Client streaming flow:");
+//var itemsToAdd = Enumerable.Range(70, 3).Select(x => new ItemMessage { Id = x, Name = $"custom name {x}", CartId = 3 });
+//var requestCall = client.AddItemToCartClientStreaming();
 
-// server streaming
+//foreach (var item in itemsToAdd) {
+//	await requestCall.RequestStream.WriteAsync(item);
+//}
+
+//await requestCall.RequestStream.CompleteAsync();
+
+//AddItemsReplyMessage clientStreamingResponse = await requestCall.ResponseAsync;
+//Console.WriteLine($"Added {clientStreamingResponse.AddedItemsCount} items");
+#endregion client streaming
+
+#region server streaming
 Console.ReadKey();
 Console.WriteLine("Server streaming flow:");
-var serverData = client.GetCartItemsServerStreaming(new CartItemsRequestMessage { CartId = 4 });
+var serverData = client.GetCartItemsServerStreaming(new CartItemsRequestMessage { CartId = 5 }); //3
 var responseStream = serverData.ResponseStream;
 
 while (await responseStream.MoveNext(new CancellationToken())) {
@@ -35,10 +51,12 @@ while (await responseStream.MoveNext(new CancellationToken())) {
 	ItemMessage response = responseStream.Current;
 	Console.WriteLine(response.Id);
 }
+#endregion server streaming
 
 Console.WriteLine("Unary flow:");
 
-// unary call
-var reply = client.GetCartItemsUnary(new CartItemsRequestMessage { CartId = 4 });
-Console.WriteLine("finish");
-Console.ReadKey();
+#region unary call
+//var reply = client.GetCartItemsUnary(new CartItemsRequestMessage { CartId = 3 });
+//Console.WriteLine("finish");
+//Console.ReadKey();
+#endregion unary call
